3e3df98ff58dc9fc3b2a29f35a0003c95814f99d
Hakim Firman <hakimastina@gmail.com>
2023-12-13 01:32:48 +0700

    FIX:composer install error

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\countLab::class,
+
         App\Providers\countPerbaikan::class,
         App\Providers\countPerbaikanTKJT::class,
     ])->toArray(),
